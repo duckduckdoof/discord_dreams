a3d0717ff0a036addcf8502d86c9f7f58b40f96f
duckduckdoof <duckduckdoof@gmail.com>
2021-12-22 17:51:49 -0500

    Cleaned up with comments

--- discord_yt_audio_bot.py
+++ discord_yt_audio_bot.py
@@ -43,42 +43,55 @@ bot = commands.Bot( command_prefix="m!", intents=intents,
 # NOTE: the below command downloads the entire video before playing; streaming seems to be better
 #---------------------------
 
-@bot.command( name='play_old', help='Plays a video/song on Youtube' )
-async def play( ctx, url ):
-    if url == None or url == "":
-        await ctx.send( "Please provide a valid Youtube URL" )
-        return
-    try:
-        server = ctx.message.guild
-        voice_channel = server.voice_client
-
-        async with ctx.typing():
-            print( "Retrieving audio..." )
-            filename = await class_utils.YTDLSource.from_url( url, ytdl, ffmpeg_options, loop=bot.loop )
-            print( "Playing audio..." )
-            voice_channel.play( discord.FFmpegPCMAudio( source=filename ) )
-    except:
-        await ctx.send( "The bot is not currently connected to a voice channel" )
+# @bot.command( name='play_old', help='Plays a video/song on Youtube' )
+# async def play( ctx, url ):
+#     if url == None or url == "":
+#         await ctx.send( "Please provide a valid Youtube URL" )
+#         return
+#     try:
+#         server = ctx.message.guild
+#         voice_channel = server.voice_client
+# 
+#         async with ctx.typing():
+#             print( "Retrieving audio..." )
+#             filename = await class_utils.YTDLSource.from_url( url, ytdl, ffmpeg_options, loop=bot.loop )
+#             print( "Playing audio..." )
+#             voice_channel.play( discord.FFmpegPCMAudio( source=filename ) )
+#     except:
+#         await ctx.send( "The bot is not currently connected to a voice channel" )
 
 #---------------------------
 
+"""
+Your basic 'play' button.
+Plays the YT video from the provided URL in the message.
+"""
 @bot.command( name='play', help='Streams a video/song on Youtube' )
 async def stream( ctx, url ):
+
+    # Sanity check those URLs
+    # TODO: a more robust URL checker could prove useful here...
     if url == None or url == "":
         await ctx.send( "Please provide a valid Youtube URL" )
         return
+
+    # Obtain the appropriate voice channel, then stream the video
     try:
         server = ctx.message.guild
         voice_channel = server.voice_client
 
         async with ctx.typing():
+            print( "Youtube video requested by " + ctx.message.author.display_name )
             print( "Retrieving audio stream..." )
             filename = await class_utils.YTDLSource.from_url( url, ytdl, ffmpeg_options, loop=bot.loop, stream=True )
             print( "Playing audio stream..." )
-            voice_channel.play( discord.FFmpegPCMAudio( source=filename ) )
+            voice_channel.play( discord.FFmpegPCMAudio( source=filename ), after=lambda: print("All done!") )
     except:
         await ctx.send( "The bot is not currently connected to a voice channel" )
 
+"""
+Pauses the bot voice client.
+"""
 @bot.command( name='pause', help='Pause the video/song' )
 async def pause( ctx ):
     voice_client = ctx.message.guild.voice_client
@@ -87,6 +100,9 @@ async def pause( ctx ):
     else:
         await ctx.send( "The bot is not playing anything at the moment." )
     
+"""
+Resumes the bot voice client (if applicable).
+"""
 @bot.command( name='resume', help='Resumes the video/song' )
 async def resume( ctx ):
     voice_client = ctx.message.guild.voice_client
@@ -95,14 +111,20 @@ async def resume( ctx ):
     else:
         await ctx.send( "The bot was not playing anything before this. Use play command" )
 
+"""
+Makes the bot stop playing the song.
+"""
 @bot.command( name='stop', help='Stops the video/song' )
-async def stop(ctx):
+async def stop( ctx ):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_playing():
         voice_client.stop()
     else:
         await ctx.send( "The bot is not playing anything at the moment." )
 
+"""
+Manual command for making the bot join the channel the invoking user is in.
+"""
 @bot.command( name='join', help='Join the channel which the invoking user is in')
 async def join( ctx ):
     if not ctx.message.author.voice:
@@ -112,6 +134,9 @@ async def join( ctx ):
         channel = ctx.message.author.voice.channel
     await channel.connect()
 
+"""
+Manual command for making the bot leave the channel.
+"""
 @bot.command( name='leave', help='Leave the voice channel (if applicable)' )
 async def leave( ctx ):
     voice_client = ctx.message.guild.voice_client
@@ -120,7 +145,10 @@ async def leave( ctx ):
     else:
         await ctx.send( "The bot is not connected to a voice channel." )
 
-@play.before_invoke
+"""
+Ensures that, if a user is in a voice channel, the bot
+will first ensure that it joins the corresponding channel before playing.
+"""
 @stream.before_invoke
 async def ensure_voice( ctx ):
     print( "Ensuring bot is in voice channel" )
@@ -133,6 +161,9 @@ async def ensure_voice( ctx ):
     elif ctx.voice_client.is_playing():
         ctx.voice_client.stop()
 
+"""
+When the bot is ready, log basic info.
+"""
 @bot.event
 async def on_ready():
     print( f'Logged in as {bot.user} (ID: {bot.user.id})' )
